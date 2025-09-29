92698f64af88f82a79b01452a1901328b7a6203a
Vova200804 <sanek_sanek_24@mail.ru>
2025-09-29 19:42:10 +0700

    Update laba.cpp

--- laba.cpp
+++ laba.cpp
@@ -1,27 +1,32 @@
-//**********************************//
-//*Имя:Солдатов Владимир Сергеевич *//
-//* Вариант:12                     *//
-//**********************************//
-      
+/********************************
+*Имя:Солдатов Владимир Сергеевич*
+*Вариант:12                     *
+*********************************/
+
 #include <iostream>
 #include <cmath>
+#include <iomanip> 
 
 using namespace std;
 
 int main() {
-    const double a = 3.244; 
-    const double b = 1.72;  
+    const double a = 3.244;
+    const double b = 1.72;
 
-    double e = sqrt(1 - b * b / (a * a));
-    double s1 = 2 * M_PI * (b * b + a * b * atan(e / sqrt(1 - e * e)) / e) / 10000; 
-    double v1 = 4 * M_PI * a * b * b / 3000000; 
-    double s2 = 2 * M_PI * (a * a + b * b * log((1 + e) / (1 - e)) / (2 * e)) / 10000; 
-    double v2 = 4 * M_PI * a * a * b / 3000000; 
+    double e, s1, v1, s2, v2;
 
-    printf("S1=%.6lf\n", s1);
-    printf("V1=%.6lf\n", v1);
-    printf("S2=%.6lf\n", s2);
-    printf("V2=%.6lf\n", v2);
+    e = sqrt(1.0 - (b * b) / (a * a));
+    s1 = 2.0 * M_PI * (b * b + a * b * atan(e / sqrt(1 - e * e)) / e) / 10000.0;
+    v1 = 4.0 * M_PI * a * b * b / 3000000.0;
+    s2 = 2.0 * M_PI * (a * a + b * b * log((1 + e) / (1 - e)) / (2 * e)) / 10000.0;
+    v2 = 4.0 * M_PI * a * a * b / 3000000.0;
+
+    cout << fixed << setprecision(6);
+    
+    cout << "S1=" << s1 << endl;
+    cout << "V1=" << v1 << endl;
+    cout << "S2=" << s2 << endl;
+    cout << "V2=" << v2 << endl;
 
     return 0;
 }
